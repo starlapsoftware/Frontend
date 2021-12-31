5cf1b2edb9d6244667893627e80f009d2fc10e00
USUARIO <USUARIO@DESKTOP-MIO1P6O>
2021-12-31 11:20:39 -0500

    2021.12.31.v2

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,13 @@
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./components/TemaConfig";
+import Contenedor from "./components/Contenedor";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-        quisquam dignissimos itaque illum, modi voluptates quasi accusamus quae
-        natus voluptatibus minus magni assumenda totam. Impedit exercitationem
-        illum eius culpa quo.
-      </h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Login />
+    </ThemeProvider>
   );
 }
 
--- reactapp/src/Components/TemaConfig.js
+++ reactapp/src/Components/TemaConfig.js
@@ -1,11 +1,11 @@
-import {createTheme} from '@mui/material/styles'
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        background: {
-          default: "#E7EBF0"
-        }
-      }
-})
+  palette: {
+    background: {
+      default: "#E7EBF0",
+    },
+  },
+});
 
 export default theme;
